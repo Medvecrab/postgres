3830b7baf500c8574ba773e8b4779c0f22432cb7
Medvecrab <mr.medvecrab@gmail.com>
2022-04-11 22:35:32 +0700

    Finalized test script

--- src/bin/pg_dump/encrypt_tests.sh
+++ src/bin/pg_dump/encrypt_tests.sh
@@ -16,10 +16,32 @@ initdb -D $DATADIR
 
 pg_ctl -D $DATADIR -l logfile start
 
-psql postgres -c "CREATE TABLE WITH ENCODING 'UTF8'"
+psql postgres -c "CREATE TABLE t2 (
+				a	integer,
+				b	integer,
+				c	integer
+)"
+
+psql postgres -c "CREATE TABLE t3 (
+				a	integer,
+				b	integer,
+				c	integer,
+				id	integer
+)"
+
+psql postgres -c "CREATE TABLE t4 (
+				id	integer
+)"
+
+psql postgres -c "CREATE TABLE t12 (
+				id	integer,
+				fio	varchar(40),
+				salary	integer
+)"
+
 
 pg_dump -t t2 --encrypt_columns "a,b,c" --encrypt "func_name" #хранимая функция везде учитывать передаваемые параметры + перегрузки функции
-pg_dump -t t3 --encrypt_columns "a,b" --encrypt "CREATE FUNCTION" #фунцкия объявляется тут
-pg_dump -t t4 --encrypt_columns "id" --encrypt "star.sql"
+pg_dump -t t3 --encrypt_columns "a,b" --encrypt "CREATE FUNCTION lorem ipsum" #фунцкия объявляется тут
+pg_dump -t t4 --encrypt_columns "id" --encrypt "sha256.sql"
 pg_dump -t t12 --encrypt "star.sql" #--encrypt по отдельности шифрует ВСЕ столбцы, а --encrypt_columns - предупреждение - отдельно от encrypt ничего не делает
 
